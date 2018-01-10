835e7787bb1c5764158de9b554c88a3f958fa525
SydBal <dominic.balassone@gmail.com>
2018-01-09 17:33:49 -0800

    Modal has a grid inside now, not very happy about it, but it'll do for now

--- frontend/components/Examples/ModalExample/index.js
+++ frontend/components/Examples/ModalExample/index.js
@@ -11,7 +11,7 @@ class ModalExample extends React.Component {
 		return (
             <div>
                 <h1>Modal Example</h1>
-
+                <br/>
                 <button type="button" className="btn btn-outline-primary" data-toggle="modal" data-target="#exampleModal">
                     Launch demo modal
                 </button>
@@ -25,16 +25,36 @@ class ModalExample extends React.Component {
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
-                            <div className="modal-body">
-                                <h5>Popover in a modal</h5>
-                                <p>This <a href="#" role="button" className="btn btn-secondary popover-test" title="Popover title" data-content="Popover body content is set in this attribute.">button</a> triggers a popover on click.</p>
-                                <hr/>
-                                <h5>Tooltips in a modal</h5>
-                                <p><a href="#" className="tooltip-test" title="Tooltip">This link</a> and <a href="#" className="tooltip-test" title="Tooltip">that link</a> have tooltips on hover.</p>
+                            <div class="modal-body">
+                                <div className="container-fluid">
+                                    <div className="row">
+                                        <div className="col-md-4">.col-md-4</div>
+                                        <div className="col-md-4 ml-auto">.col-md-4 .ml-auto</div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-md-3 ml-auto">.col-md-3 .ml-auto</div>
+                                        <div className="col-md-2 ml-auto">.col-md-2 .ml-auto</div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-md-6 ml-auto">.col-md-6 .ml-auto</div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-sm-9">
+                                            Level 1: .col-sm-9
+                                            <div className="row">
+                                                <div className="col-8 col-sm-6">
+                                                    Level 2: .col-8 .col-sm-6
+                                                </div>
+                                                <div className="col-4 col-sm-6">
+                                                    Level 2: .col-4 .col-sm-6
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                             <div className="modal-footer">
                                 <button type="button" className="btn btn-outline-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" className="btn btn-outline-primary">Save changes</button>
                             </div>
                         </div>
                     </div>
